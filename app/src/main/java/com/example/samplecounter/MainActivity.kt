9960b54eb994a833f11ff622afcbb867400b2a9d
shambu <shambuchandrannair@gmail.com>
2024-03-02 19:41:52 +0530

    second commit

--- app/src/main/java/com/example/samplecounter/MainActivity.kt
+++ app/src/main/java/com/example/samplecounter/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.samplecounter
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +19,28 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val count = findViewById<TextView>(R.id.counttxt)
+        val minus = findViewById<Button>(R.id.minusbtn)
+        val plus = findViewById<Button>(R.id.plusbtn)
+        val reset = findViewById<Button>(R.id.resetbtn)
+        var counter = 0
+        minus.setOnClickListener {
+            if (counter != 0) {
+                counter--
+                count.text = counter.toString()
+            } else {
+                Toast.makeText(this, "can't go back", Toast.LENGTH_LONG).show()
+            }
+        }
+        plus.setOnClickListener {
+            counter++
+            count.text = counter.toString()
+        }
+        reset.setOnClickListener {
+            counter = 0
+            count.text = "START"
+        }
+
     }
 }
